5409bf89774469ad5c066f390aa949e96b0d5120
CoA <1109673069@qq.com>
2025-01-13 08:18:52 +0800

    Add more API Still WIP

--- engine.c
+++ engine.c
@@ -56,3 +56,93 @@ void putimage1bit(char *image, int x, int y, int w, int h) {
 void putimagerle(char *image, int x, int y, int w, int h) {
     drawImgRLE((unsigned char *)image, x, y, w, h);
 }
+
+int spritegetvalue(int n, enum sprite_attr type) {
+    return getSpriteValue(n, type);
+}
+
+void spritesetvalue(int n, enum sprite_attr type, int value) {
+    setSpriteValue(n, type, value);
+}
+
+int angbetweenspr(int n1, int n2) { return angleBetweenSprites(n1, n2); }
+
+void getsprite(int n, int address) { setSpr(n, address); }
+
+void putsprite(int n, int x, int y) {
+    setSprPosition(n, x, y);
+    if (getSpriteValue(n, S_LIVES) < 1) {
+        setSpriteValue(n, S_LIVES, 1);
+    }
+}
+
+void spritespeed(int n, int speed, int direction) {
+    spriteSetDirectionAndSpeed(n, speed, direction);
+}
+
+int getspriteinxy(int x, int y) { return getSpriteInXY(x, y); }
+
+void putpixel(int x, int y) { setPix(x, y, color); }
+
+int getpixel(int x, int y) { return getPix(x, y); }
+
+void line(int x, int y, int x1, int y1) { drwLine(x1, y1, x1, y1); }
+
+void rect(int x, int y, int x1, int y1) { drwRect(x, y, x1, y1); }
+
+void fillrect(int x, int y, int x1, int y1) { fllRect(x, y, x1, y1); }
+
+void triangle(int x, int y, int x1, int y1, int x2, int y2) {
+    drwTriangle(x, y, x1, y1, x2, y2);
+}
+
+void filltriangle(int x, int y, int x1, int y1, int x2, int y2) {
+    fllTriangle(x, y, x1, y1, x2, y2);
+}
+
+void circle(int x, int y, int r) { drwCirc(x, y, r); }
+
+void fillcircle(int x, int y, int r) { fllCirc(x, y, r); }
+char putchar(char c) {
+    printc(c, color, bgcolor);
+    return c;
+}
+
+int puts(char *message) {
+    int j = 0;
+    while (*(message + j) != 0 && j <= 1000) {
+        printc(*(message + j), color, bgcolor);
+    }
+    return 0;
+}
+
+// FIXME: Klib hack. Get rid of it.
+int klib__itoa(int num, char *str, int radix);
+#define itoa klib__itoa
+int putn(int number) {
+    char s_buffer[7];
+    if (number < 32768) {
+        itoa(number, s_buffer, 10);
+    } else {
+        itoa(number - 0x10000, s_buffer, 10);
+    }
+    int j = 0;
+    while (s_buffer[j]) {
+        printc(s_buffer[j], color, bgcolor);
+        j++;
+    }
+    return 0;
+}
+
+int printf(char *formatString, arg - list...) {}
+
+int getchar() {}
+
+void loadfont(char *font, char first, char end) {}
+
+void setfontsize(int picture_width, int picture_height, int character_width,
+                 int character_height) {}
+
+void drawstring(char *text, int x, int y) {}
+
+void drawchar(char character, int x, int y) {}
--- screen.c
+++ screen.c
@@ -1,6 +1,7 @@
 #include "screen.h"
 #include "engine.h"
-#include <stdint.h>
+#include "font_a.h"
+#include <klib-macros.h>
 
 // This file includes functions to control the canvas in engine
 
@@ -11,6 +12,8 @@
 #define SET_LINE_IS_DRAW(a) line_is_draw[(a) >> 5] |= (1 << ((a) & 31))
 #define PARTICLE_COUNT 32
 #define SPRITE_COUNT 32
+#define MULTIPLY_FP_RESOLUTION_BITS 8
+#define abs(x) ((x) > 0 ? (x) : -(x))
 /* screen and sprite_screen
  * 7        4 3        0
  * ┌─────────┬─────────┐
@@ -38,6 +41,7 @@ uint8_t clipy1 = 128;
 uint8_t isClip = 0;
 int8_t regx = 0;
 int8_t regy = 0;
+char charArray[340];
 extern int8_t bgcolor;
 extern int8_t color;
 
@@ -359,7 +363,7 @@ void drawImageBit(uint8_t *image, int16_t x1, int16_t y1, int16_t w,
         return;
     }
     int16_t i = 0;
-    uint8_t ibit;
+    uint8_t ibit = 0;
     for (int16_t y = 0; y < h; y++)
         for (int16_t x = 0; x < w; x++) {
             if (i % 8 == 0) {
@@ -479,32 +483,525 @@ void drawImgRLE(uint8_t *image, int16_t x1, int16_t y1, int16_t w, int16_t h) {
     }
 }
 
-int16_t getSpriteValue(uint16_t n, uint16_t t) {
-    if (n > SPRITE_COUNT)
+int16_t getSpriteValue(uint16_t n, SpriteAttribute t) {
+    if (n >= SPRITE_COUNT)
         return 0;
     switch (t) {
-    case 0:
+    case S_X:
         return sprite_table[n].x >> 2;
-    case 1:
+    case S_Y:
         return sprite_table[n].y >> 2;
-    case 2:
+    case S_SPEEDX:
         return sprite_table[n].speedx;
-    case 3:
+    case S_SPEEDY:
         return sprite_table[n].speedy;
-    case 4:
+    case S_WIDTH:
         return sprite_table[n].width;
-    case 5:
+    case S_HEIGHT:
         return sprite_table[n].height;
-    case 6:
+    case S_ANGLE:
         return sprite_table[n].angle;
-    case 7:
+    case S_LIVES:
         return sprite_table[n].lives;
-    case 8:
+    case S_COLLISION:
         return sprite_table[n].collision;
-    case 9:
-        return SPRITE_IS_SCROLLED(n); // todo：为什么这里是这个 9 是 s_solid才对
-    case 10:
+    case S_SOLID:
+        return SPRITE_IS_SOLID(n);
+    case S_GRAVITY:
         return sprite_table[n].gravity;
+    default: // TODO: too lazy to add the remainings now
+        return 0;
     }
     return 0;
+}
+
+void setSpriteValue(uint16_t n, SpriteAttribute t, int16_t v) {
+    if (n >= SPRITE_COUNT)
+        return;
+    switch (t) {
+    case S_X:
+        sprite_table[n].x = v << 2;
+        return;
+    case S_Y:
+        sprite_table[n].y = v << 2;
+        return;
+    case S_SPEEDX:
+        sprite_table[n].speedx = (int8_t)v;
+        return;
+    case S_SPEEDY:
+        sprite_table[n].speedy = (int8_t)v;
+        return;
+    case S_WIDTH:
+        sprite_table[n].width = v;
+        return;
+    case S_HEIGHT:
+        sprite_table[n].height = v;
+        return;
+    case S_ANGLE:
+        v = v % 360;
+        if (v < 0)
+            v += 360;
+        sprite_table[n].angle = v;
+        return;
+    case S_LIVES:
+        sprite_table[n].lives = v;
+        return;
+    case S_COLLISION:
+        return;
+    case S_SOLID:
+        if (v != 0)
+            sprite_table[n].flags |= 0x01;
+        else
+            sprite_table[n].flags &= ~0x01;
+        return;
+    case S_GRAVITY:
+        sprite_table[n].gravity = v;
+        return;
+    case S_ON_COLLISION:
+        sprite_table[n].oncollision = (uint16_t)v;
+        return;
+    case S_ON_EXIT_SCREEN:
+        sprite_table[n].onexitscreen = (uint16_t)v;
+        return;
+    case S_IS_SCROLLED:
+        if (v != 0)
+            sprite_table[n].flags |= 0x02;
+        else
+            sprite_table[n].flags &= ~0x02;
+        return;
+    case S_IS_ONEBIT:
+        if (v != 0)
+            sprite_table[n].flags |= 0x04;
+        else
+            sprite_table[n].flags &= ~0x04;
+        return;
+    case S_FLIP_HORIZONTAL: // TODO: where does this 15 16 17 comes from
+        if (v != 0)
+            sprite_table[n].flags |= 0x08;
+        else
+            sprite_table[n].flags &= ~0x08;
+        return;
+    case S_Z_INDEX:
+        sprite_table[n].zindex = (uint8_t)v;
+        return;
+    case S_COLOR:
+        sprite_table[n].flags &= 0x0f;
+        sprite_table[n].flags |= (uint8_t)v << 4;
+        return;
+    }
+}
+int16_t atan2_fp(int16_t y_fp, int16_t x_fp) {
+    int32_t coeff_1 = 45;
+    int32_t coeff_1b = -56; // 56.24;
+    int32_t coeff_1c = 11;  // 11.25
+    int16_t coeff_2 = 135;
+    int16_t angle = 0;
+    int32_t r;
+    int32_t r3;
+    int16_t y_abs_fp = y_fp;
+    if (y_abs_fp < 0)
+        y_abs_fp = -y_abs_fp;
+    if (y_fp == 0) {
+        if (x_fp >= 0) {
+            angle = 0;
+        } else {
+            angle = 180;
+        }
+    } else if (x_fp >= 0) {
+        r = (((int32_t)(x_fp - y_abs_fp)) << MULTIPLY_FP_RESOLUTION_BITS) /
+            ((int32_t)(x_fp + y_abs_fp));
+        r3 = r * r;
+        r3 = r3 >> MULTIPLY_FP_RESOLUTION_BITS;
+        r3 *= r;
+        r3 = r3 >> MULTIPLY_FP_RESOLUTION_BITS;
+        r3 *= coeff_1c;
+        angle = (int16_t)(coeff_1 +
+                          ((coeff_1b * r + r3) >> MULTIPLY_FP_RESOLUTION_BITS));
+    } else {
+        r = (((int32_t)(x_fp + y_abs_fp)) << MULTIPLY_FP_RESOLUTION_BITS) /
+            ((int32_t)(y_abs_fp - x_fp));
+        r3 = r * r;
+        r3 = r3 >> MULTIPLY_FP_RESOLUTION_BITS;
+        r3 *= r;
+        r3 = r3 >> MULTIPLY_FP_RESOLUTION_BITS;
+        r3 *= coeff_1c;
+        angle =
+            coeff_2 +
+            ((int16_t)(((coeff_1b * r + r3) >> MULTIPLY_FP_RESOLUTION_BITS)));
+    }
+    if (y_fp < 0)
+        return (-angle); // negate if in quad III or IV
+    else
+        return (angle);
+}
+int16_t fixed_sin(int x) {
+    // Bhaskara I's sine approximation sin(x°) = 4·x·(180−x)/(40500−x·(180−x))
+    char pos = 1; // positive - keeps an eye on the sign.
+    if (x < 0) {
+        x = -x;
+        pos = !pos;
+    }
+    if (x >= 360)
+        x %= 360;
+    if (x > 180) {
+        x -= 180;
+        pos = !pos;
+    }
+    int16_t nv = x * (180 - x);
+    int32_t s = (nv * 4 * (1 << fixed_res_bit)) / (40500 - nv);
+    if (pos)
+        return (int16_t)s;
+    return (int16_t)-s;
+}
+int16_t fixed_cos(int16_t g) { return fixed_sin(g + 90); }
+
+int16_t angleBetweenSprites(uint16_t n1, uint16_t n2) {
+    if (n1 >= SPRITE_COUNT || n2 >= SPRITE_COUNT)
+        return 0;
+    int16_t A = atan2_fp(sprite_table[n1].y - sprite_table[n2].y,
+                         sprite_table[n1].x - sprite_table[n2].x);
+    A = (A < 0) ? A + 360 : A;
+    return A;
+}
+void setSpr(uint16_t n, uint16_t adr) {
+    if (n >= SPRITE_COUNT)
+        return;
+    sprite_table[n].address = adr;
+}
+
+void setSprPosition(uint16_t n, uint16_t x, uint16_t y) {
+    if (n >= SPRITE_COUNT)
+        return;
+    sprite_table[n].x = x << 2;
+    sprite_table[n].y = y << 2;
+    sprite_table[n].previousx = x << 2;
+    sprite_table[n].previousy = y << 2;
+}
+
+void spriteSetDirectionAndSpeed(uint16_t n, uint16_t speed, int16_t dir) {
+    if (n >= SPRITE_COUNT)
+        return;
+    dir = dir % 360;
+    if (dir < 0)
+        dir += 360;
+    sprite_table[n].speedx = ((speed * fixed_cos(dir)) >> fixed_res_bit);
+    sprite_table[n].speedy = ((speed * fixed_sin(dir)) >> fixed_res_bit);
+}
+
+int16_t getSpriteInXY(int16_t x, int16_t y) {
+    for (int n = 0; n < SPRITE_COUNT; n++) {
+        if (sprite_table[n].lives > 0)
+            if ((sprite_table[n].x >> 2) < x &&
+                (sprite_table[n].x >> 2) + sprite_table[n].width > x &&
+                (sprite_table[n].y >> 2) < y &&
+                (sprite_table[n].y >> 2) + sprite_table[n].height > y)
+                return n;
+    }
+    return -1;
+}
+uint8_t getPix(uint8_t x, uint8_t y) {
+    uint16_t xi, b;
+    if (x < SCREEN_WIDTH && y < SCREEN_HEIGHT) {
+        xi = x / 2;
+        if (x % 2)
+            b = (screen[SCREEN_ADDR(xi, y)] & 0x0f);
+        else
+            b = (screen[SCREEN_ADDR(xi, y)] & 0xf0) >> 4;
+        return b;
+    }
+    return 0;
+}
+
+void drawFVLine(int x, int y1, int y2) {
+    for (int i = y1; i <= y2; i++)
+        setPix(x, i, color);
+}
+void drawFHLine(int16_t x1, int16_t x2, uint16_t y) {
+    uint8_t *nPtr, c;
+    uint16_t i;
+    if (isClip) {
+        if (y < clipy0 || y >= clipy1)
+            return;
+        if (x1 < clipx0)
+            x1 = clipx0;
+        if (x2 >= clipx1)
+            x2 = clipx1;
+    } else {
+        if (y > 127)
+            return;
+        if (x1 < 0)
+            x1 = 0;
+        if (x2 >= 127)
+            x2 = 127;
+    }
+    if (x1 & 1) {
+        screen[SCREEN_ADDR(x1 / 2, y)] =
+            (screen[SCREEN_ADDR(x1 / 2, y)] & 0xf0) + color;
+        x1++;
+    }
+    if (!(x2 & 1)) {
+        screen[SCREEN_ADDR(x2 / 2, y)] =
+            (screen[SCREEN_ADDR(x2 / 2, y)] & 0x0f) + (color << 4);
+        x2--;
+    }
+    SET_LINE_IS_DRAW(y);
+    i = x1 / 2;
+    c = (color << 4) + color;
+    nPtr = (uint8_t *)&screen[SCREEN_ADDR(i, y)];
+    for (; i <= x2 / 2; i++)
+        *nPtr++ = c;
+}
+void drwLine(int16_t x1, int16_t y1, int16_t x2, int16_t y2) {
+    if (x1 == x2) {
+        if (y1 > y2)
+            drawFVLine(x1, y2, y1);
+        else
+            drawFVLine(x1, y1, y2);
+        return;
+    } else if (y1 == y2) {
+        if (x1 > x2)
+            drawFHLine(x2, x1, y1);
+        else
+            drawFHLine(x1, x2, y1);
+        return;
+    }
+    int deltaX = abs(x2 - x1);
+    int deltaY = abs(y2 - y1);
+    int signX = x1 < x2 ? 1 : -1;
+    int signY = y1 < y2 ? 1 : -1;
+    int error = deltaX - deltaY;
+    int error2;
+    setPix(x2, y2, color);
+    while (x1 != x2 || y1 != y2) {
+        setPix(x1, y1, color);
+        error2 = error * 2;
+        if (error2 > -deltaY) {
+            error -= deltaY;
+            x1 += signX;
+        }
+        if (error2 < deltaX) {
+            error += deltaX;
+            y1 += signY;
+        }
+    }
+}
+
+void drwRect(int16_t x0, int16_t y0, int16_t x1, int16_t y1) {
+    drawFHLine(x0, x1, y0);
+    drawFHLine(x0, x1, y1);
+    drawFVLine(x0, y0, y1);
+    drawFVLine(x1, y0, y1);
+}
+void fllRect(int16_t x0, int16_t y0, int16_t x1, int16_t y1) {
+    for (int16_t jy = y0; jy <= y1; jy++)
+        drawFHLine(x0, x1, jy);
+}
+
+void drwTriangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2,
+                 uint16_t x3, uint16_t y3) {
+    drwLine(x1, y1, x2, y2);
+    drwLine(x2, y2, x3, y3);
+    drwLine(x3, y3, x1, y1);
+}
+void fllTriangle(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1,
+                 uint16_t x2, uint16_t y2) {
+    int32_t a, b, y, last, t;
+
+    // Sort coordinates by Y order (y2 >= y1 >= y0)
+    if (y0 > y1) {
+        t = y0;
+        y0 = y1;
+        y1 = t;
+        t = x0;
+        x0 = x1;
+        x1 = t;
+    }
+    if (y1 > y2) {
+        t = y2;
+        y2 = y1;
+        y1 = t;
+        t = x2;
+        x2 = x1;
+        x1 = t;
+    }
+    if (y0 > y1) {
+        t = y0;
+        y0 = y1;
+        y1 = t;
+        t = x0;
+        x0 = x1;
+        x1 = t;
+    }
+
+    if (y0 == y2) {
+        a = b = x0;
+        if (x1 < a)
+            a = x1;
+        else if (x1 > b)
+            b = x1;
+        if (x2 < a)
+            a = x2;
+        else if (x2 > b)
+            b = x2;
+        drawFHLine(a, b, y0);
+        return;
+    }
+
+    int32_t dx01 = x1 - x0, dy01 = y1 - y0, dx02 = x2 - x0, dy02 = y2 - y0,
+            dx12 = x2 - x1, dy12 = y2 - y1, sa = 0, sb = 0;
+
+    if (y1 == y2)
+        last = y1;
+    else
+        last = y1 - 1;
+
+    for (y = y0; y <= last; y++) {
+        a = x0 + sa / dy01;
+        b = x0 + sb / dy02;
+        sa += dx01;
+        sb += dx02;
+
+        if (a > b) {
+            t = a;
+            a = b;
+            b = t;
+        }
+        drawFHLine(a, b, y);
+    }
+
+    // For lower part of triangle, find scanline crossings for segments
+    // 0-2 and 1-2.  This loop is skipped if y1=y2.
+    sa = dx12 * (y - y1);
+    sb = dx02 * (y - y0);
+    for (; y <= y2; y++) {
+        a = x1 + sa / dy12;
+        b = x0 + sb / dy02;
+        sa += dx12;
+        sb += dx02;
+
+        if (a > b) {
+            t = a;
+            a = b;
+            b = t;
+        }
+        drawFHLine(a, b, y);
+    }
+}
+void drwCirc(int16_t x0, int16_t y0, int16_t r) {
+    int16_t x = 0;
+    int16_t dx = 1;
+    int16_t dy = r + r;
+    int16_t p = -(r >> 1);
+
+    // These are ordered to minimise coordinate changes in x or y
+    // drawPixel can then send fewer bounding box commands
+    setPix(x0 + r, y0, color);
+    setPix(x0 - r, y0, color);
+    setPix(x0, y0 - r, color);
+    setPix(x0, y0 + r, color);
+
+    while (x < r) {
+
+        if (p >= 0) {
+            dy -= 2;
+            p -= dy;
+            r--;
+        }
+
+        dx += 2;
+        p += dx;
+
+        x++;
+
+        // These are ordered to minimise coordinate changes in x or y
+        // drawPixel can then send fewer bounding box commands
+        setPix(x0 + x, y0 + r, color);
+        setPix(x0 - x, y0 + r, color);
+        setPix(x0 - x, y0 - r, color);
+        setPix(x0 + x, y0 - r, color);
+
+        setPix(x0 + r, y0 + x, color);
+        setPix(x0 - r, y0 + x, color);
+        setPix(x0 - r, y0 - x, color);
+        setPix(x0 + r, y0 - x, color);
+    }
+}
+void fllCirc(int16_t x0, int16_t y0, int16_t r) {
+    int16_t x = 0;
+    int16_t dx = 1;
+    int16_t dy = r + r;
+    int16_t p = -(r >> 1);
+
+    drawFHLine(x0 - r, x0 + r, y0);
+
+    while (x < r) {
+        if (p >= 0) {
+            dy -= 2;
+            p -= dy;
+            r--;
+        }
+
+        dx += 2;
+        p += dx;
+
+        x++;
+
+        drawFHLine(x0 - r, x0 + r, y0 + x);
+        drawFHLine(x0 - r, x0 + r, y0 - x);
+        drawFHLine(x0 - x, x0 + x, y0 + r);
+        drawFHLine(x0 - x, x0 + x, y0 - r);
+    }
+}
+
+void drawChar(char c, uint8_t x, uint8_t y) {
+    for (int8_t i = 0; i < 5; i++) { // Char bitmap = 5 columns
+        uint8_t line = (font_a[c * 5 + i]);
+        for (int8_t j = 0; j < 8; j++, line >>= 1) {
+            if (line & 1)
+                setPix(x + i, y + j, color);
+        }
+    }
+}
+void charLineUp(uint8_t n) {
+    clearScr(bgcolor);
+    for (uint16_t i = 0; i < 336 - n * 21; i++) {
+        charArray[i] = charArray[i + n * 21];
+        drawChar(charArray[i], (i % 21) * 6, (i / 21) * 8);
+    }
+}
+
+inline int8_t getCharY() { return regy; }
+
+void printc(char c, uint8_t fc, uint8_t bc) {
+    if (regy > 15) {
+        regy = 15;
+        charLineUp(1);
+    }
+    if (c == '\n') {
+        fillRect(regx * 6, regy * 8, 127 - regx * 6, 8, bgcolor);
+        for (uint8_t i = regx; i <= 21; i++) {
+            charArray[regx + regy * 21] = ' ';
+        }
+        regy++;
+        regx = 0;
+    } else if (c == '\t') {
+        for (uint8_t i = 0; i <= regx % 5; i++) {
+            fillRect(regx * 6, regy * 8, 6, 8, bgcolor);
+            charArray[regx + regy * 21] = ' ';
+            regx++;
+            if (regx > 20) {
+                regy++;
+                regx = 0;
+            }
+        }
+    } else {
+        fillRect(regx * 6, regy * 8, 6, 8, bgcolor);
+        drawChar(c, regx * 6, regy * 8);
+        charArray[regx + regy * 21] = c;
+        regx++;
+        if (regx > 20) {
+            regy++;
+            regx = 0;
+        }
+    }
 }
